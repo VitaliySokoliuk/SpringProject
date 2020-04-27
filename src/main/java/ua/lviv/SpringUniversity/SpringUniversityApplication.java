8fdd39db19f5f0b97c9d8a13c16d9ede7aaf6a04
Vitaliy Sokoliuk <Vit.sokol0@gmail.com>
2020-04-27 22:07:43 +0300

    Initial structure

---
+++ src/main/java/ua/lviv/SpringUniversity/SpringUniversityApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.SpringUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/SpringUniversity/SpringUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.SpringUniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
